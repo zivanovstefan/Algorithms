60ba82098e3eb4f52137b52d0ab103d5bfcb52ee
Stefan Zivanov <zivanovstefan@gmail.com>
2022-02-21 16:15:21 +0100

    SOLID principles applied to PalindromeChecker

---
+++ Algorithms/PalindromeChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class PalindromeChecker : IChecker
+    {
+        public bool Check(string userinput)
+        {
+			char[] charArray = userinput.ToCharArray();
+			//reversing an array
+			Array.Reverse(charArray);
+			//converting charArray to string
+			string reversedInput = new string(charArray);
+			//is user input equal to the reversed user input
+			if (userinput.Equals(reversedInput))
+			{
+				return true;
+			}
+			return false;
+		}
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,43 +1,12 @@
 ﻿using System;
+using Algorithms;
 
-namespace PalindromeChecker
-{
-	class Program
-	{
-		public static void Main()
-		{
-			// Asking the user to enter a word, number or phrase
-			Console.WriteLine("Enter a word, number or phrase...\n");
-			string input = Console.ReadLine();
-			bool status = false;
+// Asking the user to enter a word, number or phrase
+Console.WriteLine("Enter a word, number or phrase...\n");
+var input = Console.ReadLine(); 
 
-			//converting characters to lower case
-			string lowercaseInput = input.ToLower();
+//converting characters to lower case
+var checker = new PalindromeChecker();
+var check = checker.Check(input);
 
-			// Printing result
-			if (isPalindrome(lowercaseInput))
-			{
-				status = true;
-				Console.WriteLine(status);
-			}
-			else
-			{
-				Console.WriteLine(status);
-			}
-		}
-		public static bool isPalindrome(string userinput)
-		{
-			char[] charArray = userinput.ToCharArray();
-			//reversing an array
-			Array.Reverse(charArray);
-			//converting charArray to string
-			string reversedInput = new string(charArray);
-			//is user input equal to the reversed user input
-			if (userinput.Equals(reversedInput))
-			{
-				return true;
-			}
-			return false;
-		}
-	}
-}
+Console.WriteLine(check);
