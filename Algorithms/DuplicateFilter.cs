465cf659636067f73ed518e9eba90dd66c729a2b
Stefan Zivanov <zivanovstefan@gmail.com>
2022-02-21 17:05:12 +0100

    SRP applied

---
+++ Algorithms/DuplicateFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class DuplicateFilter
+    {
+        public string Filter(string s)
+        {
+            string str = "";
+
+            for (int stringIndex = 0; stringIndex < s.Length; stringIndex++)
+            {
+                char letter = s[stringIndex];
+                if (str.IndexOf(letter) < 0)
+                {
+                    str += letter;
+                }
+            }
+            return str;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,63 +1,25 @@
 ﻿using System;
+using Algorithms;
 
-namespace Algorithms
+Console.WriteLine("Enter a word");
+string str = Console.ReadLine();
+
+Console.WriteLine("Insert a for the number of occurrences of letters in a word");
+Console.WriteLine("Insert b for string without duplicates");
+
+var counter = new LetterOccurrenceCounter();
+var filter = new DuplicateFilter();
+
+string userChoice = Console.ReadLine();
+if (userChoice == "a")
+      {
+          Console.WriteLine(counter.Count(str));
+      }
+else if (userChoice == "b")
+      {
+      Console.WriteLine(filter.Filter(str));
+       }
+else
 {
-	class Program
-	{
-		public static void Main()
-		{
-            Console.WriteLine("Enter a word");
-            string str = Console.ReadLine();
-            Console.WriteLine("Insert a for the number of occurrences of letters in a word");
-            Console.WriteLine("Insert b for string without duplicates");
-            string userChoice = Console.ReadLine();
-            if (userChoice == "a")
-            {
-                Console.WriteLine(CountLetterOccurrences(str));
-            }
-            else if (userChoice == "b")
-            {
-                Console.WriteLine(FilterLetters(str));
-            }
-            else
-            {
-                Console.WriteLine("Your choice is not valid");
-            }
-        }
-        //method for counting the number of occurences of letter in a word
-        public static string CountLetterOccurrences(string str)
-        {
-            while (str.Length > 0)
-            {
-                Console.Write(str[0]);
-                int count = 0;
-                for (int stringIndex = 0; stringIndex < str.Length; stringIndex++)
-                {
-                    if (str[0] == str[stringIndex])
-                    {
-                        count++;
-                    }
-                }
-                str = str.Replace(str[0].ToString(), string.Empty);
-                Console.WriteLine(count);
-            }
-            return Console.ReadLine();
-        }
-        //method for filtering letters
-        public static string FilterLetters(string s)
-        {
-            string str = "";
-
-            for (int stringIndex = 0; stringIndex < s.Length; stringIndex++)
-            {
-                char letter = s[stringIndex];
-                if (str.IndexOf(letter) < 0)
-                {
-                    str += letter;
-                }
-            }
-            return str;
-        }
-
-    }
+Console.WriteLine("Your choice is not valid");
 }
