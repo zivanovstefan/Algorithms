3502a46fb8a0d74fbdc5a3e9077d99b259d0d40b
Stefan Zivanov <zivanovstefan@gmail.com>
2022-02-21 12:28:27 +0100

    Palindrome checker task finished

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PalindromeChecker
+{
+	class Program
+	{
+		public static void Main()
+		{
+			// Asking the user to enter a word, number or phrase
+			Console.WriteLine("Enter a word, number or phrase...\n");
+			string input = Console.ReadLine();
+			bool status = false;
+
+			//converting characters to lower case
+			string lowercaseInput = input.ToLower();
+
+			// Printing result
+			if (isPalindrome(lowercaseInput))
+			{
+				status = true;
+				Console.WriteLine(status);
+			}
+			else
+			{
+				Console.WriteLine(status);
+			}
+		}
+		public static bool isPalindrome(string userinput)
+		{
+			char[] charArray = userinput.ToCharArray();
+			//reversing an array
+			Array.Reverse(charArray);
+			//converting charArray to string
+			string reversedInput = new string(charArray);
+			//is user input equal to the reversed user input
+			if (userinput.Equals(reversedInput))
+			{
+				return true;
+			}
+			return false;
+		}
+	}
+}
