6ceac0585fd048f4e5350a7ca96d422c08e257ec
Stefan Zivanov <zivanovstefan@gmail.com>
2022-02-22 17:30:26 +0100

    User input and string processing moved from Program.cs to new classes(User input and StringProcessor)

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,25 +1,8 @@
 ﻿using System;
 using Algorithms;
 
-Console.WriteLine("Enter a word");
-string str = Console.ReadLine();
+string word = new UserInput().GetInput();
+string option = new UserInput().GetOption();
 
-Console.WriteLine("Insert a for the number of occurrences of letters in a word");
-Console.WriteLine("Insert b for string without duplicates");
-
-var counter = new LetterOccurrenceCounter();
-var filter = new DuplicateFilter();
-
-string userChoice = Console.ReadLine();
-if (userChoice == "a")
-      {
-          Console.WriteLine(counter.Count(str));
-      }
-else if (userChoice == "b")
-      {
-      Console.WriteLine(filter.Filter(str));
-       }
-else
-{
-Console.WriteLine("Your choice is not valid");
-}
+string process = new StringProcessor().OptionProcess(word, option);
+Console.WriteLine(process);
---
+++ Algorithms/StringProcessor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class StringProcessor
+    {
+        public string OptionProcess(string word, string option)
+        {
+            var counter = new LetterOccurrenceCounter();
+            var filter = new DuplicateFilter();
+
+            if (option == "a")
+            {
+                return counter.Count(word);
+            }
+            else if (option == "b")
+            {
+                return filter.Filter(word);
+            }
+            else
+            {
+                return "Your choice is not valid";
+            }
+        }
+    }
+}
